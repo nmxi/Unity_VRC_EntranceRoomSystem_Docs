e5fa67cb86ba3110ead9ff34413467ec5bf2712a
mikinel <nagatsukimxi@gmail.com>
2025-11-01 15:38:05 +0900

    boothURL変更

--- ers-docs/docusaurus.config.ts
+++ ers-docs/docusaurus.config.ts
@@ -101,7 +101,7 @@ const config: Config = {
           items: [
             {
               label: 'Booth',
-              href: 'https://nmxi.booth.pm/items/7598569',
+              href: 'https://nmxi.booth.pm/items/7604667',
             },
           ],
         },
